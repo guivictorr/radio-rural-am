da3f9e7ecad8bd47fb8889af55b1f5266fd2d780
guivictorr <guilhermevictor20@gmail.com>
2020-08-16 13:39:02 -0300

    👌

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, ImageBackground, FlatList } from 'react-native';
 
-import styles from './styles'
 import Article from '../../components/Article';
+
+import styles from './styles'
 import api from '../../services/api';
 
 const News = () => {
